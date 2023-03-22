3eec38edf0b2567b21c8f572d29e31c9ebb24e99
Meldgaard99 <100026274+Meldgaard99@users.noreply.github.com>
2023-03-22 17:33:53 +0100

    first commit

--- wifi ping/Program.cs
+++ wifi ping/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Threading;
+using Newtonsoft.Json;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int failedPings = 0;
+        string jsonFilePath = "failed-pings.json";
+
+        while (true)
+        {
+            Ping ping = new Ping();
+             PingReply reply = ping.Send("google.com");
+
+            if (reply.Status != IPStatus.Success)
+            {
+                failedPings++;
+                var failedPingInfo = new { Number = failedPings, Status = reply.Status.ToString(), Timestamp = DateTime.Now };
+                string json = JsonConvert.SerializeObject(failedPingInfo);
+                File.AppendAllText(jsonFilePath, json + Environment.NewLine);
+                Console.WriteLine($"Failed ping #{failedPings}: {reply.Status} at {DateTime.Now}");
+            }
+            else
+            {
+                Console.WriteLine("Ping successful");
+            }
+
+            Thread.Sleep(1000); // wait for 1 second before pinging again
+        }
+    }
+}
